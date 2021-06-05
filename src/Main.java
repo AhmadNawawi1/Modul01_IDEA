0f3513d8321f09c12ad70046c7e7a7bf6209a7d9
ASUS <czxahmad122@gmail.com>
2021-06-05 12:44:59 +0800

    Operasi Tipe string

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.sql.SQLOutput;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,5 +17,21 @@ public class Main {
         char[] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5,8));
+        System.out.println(namaLengkap.replace("mad","Barr"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for(String nama : namaArray){
+            System.out.println(nama);
+        }
     }
 }
