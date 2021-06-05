1dd1a1eba87576b6af875671abf1ab9ae51813b3
ASUS <czxahmad122@gmail.com>
2021-06-05 12:20:24 +0800

    Project IDEA

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+
+        String namaDepan = "Ahmad";
+        String namaBelakang = "Nawawi";
+        int usia = 21;
+        int targetTahunKuliah = 4;
+        double ipk = 3.899;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println("Nama Lengkap : " + " " + namaDepan + " " + namaBelakang);
+    }
 }
